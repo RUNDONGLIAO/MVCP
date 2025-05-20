21b58786c04067ac4b5ec07f059198d917573ca4
Rundong Liao <151279833+RUNDONGLIAO@users.noreply.github.com>
2025-05-20 23:22:09 +0800

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# v1.0.0 (2025-04-25)
+- Version used in the submitted paper: Applied Energy
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mvcp",
+    version="0.1.0",
+    description="Model for 5P Change Points - Temperature with energy consumption analysis tool",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "openpyxl",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
